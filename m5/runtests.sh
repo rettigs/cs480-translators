4455aab5acef4c9e6c0f7a5a261280b7892f1166
Sean Rettig <seanrettig@gmail.com>
2015-03-15 23:28:48 -0700

    m5: update test runner

--- m5/runtests.sh
+++ m5/runtests.sh
@@ -4,6 +4,6 @@ TESTDIR="test"
 
 for file in `ls $TESTDIR`; do
     echo -e "Test '$file':\n"
-    cat "$TESTDIR/$file" | ./lexer.py | ./parser.py | ./parsetree.py | ./gforther.py
+    cat "$TESTDIR/$file" | ./lexer.py | ./parser.py | ./syntaxtree.py | ./gforther.py
     echo -e "\n"
 done
